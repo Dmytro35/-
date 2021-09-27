8b972f8a03006ccb2fd909f22476149e9c9f4f88
Шелестюк Дмитро <90178793+Dmytro35@users.noreply.github.com>
2021-09-27 17:41:02 +0300

    Add files via upload

---
+++ калькулятор.py
@@ -0,0 +1,25 @@
+f_num = float(input("Введите первое число>>"))
+oper = input("Введите операцию>>")
+s_num = float(input("Введите второе число>>"))
+if oper == '+':
+    print(f_num + s_num )
+elif oper == '-' :
+    print(f_num - s_num)
+elif oper == '*' :
+    print(f_num * s_num)
+elif oper == '/':
+    print(f_num / s_num)
+else:
+    print("Error")
+prodolzhit = input("Введите 'y', чтобы продолжить,или любую клавишу, чтобы завершить>> ")
+while prodolzhit == 'y' :
+    if oper == '+':
+      print(f_num + s_num )
+    elif oper == '-' :
+      print(f_num - s_num)
+    elif oper == '*' :
+      print(f_num * s_num)
+    elif oper == '/':
+      print(f_num / s_num)
+else:
+     print("Error")
